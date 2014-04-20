c743d953c6eac4ad832bd084cbd572e868a22846
Justin Kenyon <kenyonj@gmail.com>
2014-04-19 22:53:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kristaks::Application.config.secret_key_base = 'f7c0089fb444b942afcb91d47d131a752429a8b475082ee4f477e747c706ac71db14480e70494ae44feb8b42a30401e6a139f77821434225f581d81c281a81bc'
