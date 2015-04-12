e3b98b77e489259a96fb8401e29559fe24d2a3bf
Justin Kenyon <kenyonj@gmail.com>
2015-04-12 16:52:03 -0400

    Update hours

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 STORE_HOURS = [
-  ["Monday", "12PM", "11PM", false],
-  ["Tuesday", "12PM", "11PM", false],
-  ["Wednesday", "12PM", "11PM", false],
+  ["Monday", "12PM", "11PM", true],
+  ["Tuesday", "12PM", "11PM", true],
+  ["Wednesday", "12PM", "11PM", true],
   ["Thursday", "12PM", "9PM", true],
   ["Friday", "12PM", "10PM", true],
   ["Saturday", "12PM", "10PM", true],
@@ -12,8 +12,8 @@ kristaks = Store.create(
   name: "Krista K's",
   address: "882 Route 28, West Dennis, MA 02670",
   phone_number: "5083986569",
-  closing_day: DateTime.new(2014,10,14,12,0,0).in_time_zone('EST'),
-  opening_day: DateTime.new(2015,4,15,12,0,0).in_time_zone('EST')
+  closing_day: DateTime.new(2015,10,14,12,0,0).in_time_zone('EST'),
+  opening_day: DateTime.new(2015,4,17,12,0,0).in_time_zone('EST')
 )
 
 STORE_HOURS.each do |day, opens, closes, working|
